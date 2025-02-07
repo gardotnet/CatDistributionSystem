51bf6bcda95d55061357fdd74cae1d5d382a003c
gar <gargallarde@gmail.com>
2025-02-07 00:14:10 +0000

    06/02/25
    Added to the exterior tilemap, fixing player transition between levels so it spawns on exterior level at a specified point

--- CSharpTopDownFramework/Assets/Scripts/sceneTransition.cs
+++ CSharpTopDownFramework/Assets/Scripts/sceneTransition.cs
@@ -4,11 +4,14 @@ using UnityEngine.SceneManagement;
 public class sceneTransition : MonoBehaviour
 {
     public string sceneToLoad;
+    public Vector2 playerPosition;
+    public VectorValue playerStorage;
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") && !collision.isTrigger)
         {
+            playerStorage.initialValue = playerPosition;
             SceneManager.LoadScene(sceneToLoad);
         }
     }
